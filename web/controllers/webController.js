d3844670bf66e1cd21e222dfb2f0b0f1c7f93d55
paul <akwangho@gmail.com>
2014-08-06 01:25:31 +0800

    support resizable div on sidebar

--- web/controllers/webController.js
+++ web/controllers/webController.js
@@ -12,30 +12,52 @@ angular.module("cassandraWeb", ['angularTreeview'])
             }).error(function (error) {
                 $scope.error = error;
             });
+            function bin2String(array) {
+                var result = "";
+                for (var i = 0; i < array.length; i++) {
+                    result += String.fromCharCode(parseInt(array[i], 10));
+                }
+                return result;
+            }
+            function labelSort(obj1, obj2) {
+                return obj1.label > obj2.label? 1: -1;
+            }
             var genTreeData = function (keyspaces) {
                 keyspaces.forEach(function (ks) {
                     var ksInfo = [];
                     ks.cf_defs.forEach(function (cf) {
-                        console.log(cf);
                         var cfInfo = [{
                             label: "ColumnType: " + cf.column_type
                         }, {
                             label: "ComparatorType: " + cf.comparator_type
+                        }, {
+                            label: "MaxCompactionThreshold: " + cf.max_compaction_threshold
+                        }, {
+                            label: "MinCompactionThreshold: " + cf.min_compaction_threshold
+                        }, {
+                            label: "RowCacheSize: " + cf.row_cache_size
+                        }, {
+                            label: "DefaultValidationClass: " + cf.default_validation_class
                         }];
-//                    for (var c in cf) {
-//                        console.log(c);
-//                        c == 'ColumnType' && cfInfo.push({
-//                            "label": c  + ': ' + JSON.stringify(cf[c])
-//                        });
-//                    }
+                        var cfdInfo = [];
+                        cf.column_metadata.forEach(function (cfd) {
+                            cfdInfo.push({
+                                label: bin2String(cfd.name)
+                            })
+                        });
+                        cf.column_metadata && cf.column_metadata.length > 0 &&
+                            cfInfo.push({
+                                label: "Column Definitions",
+                                children: cfdInfo.sort(labelSort),
+                                collapsed: true
+                            });
                         ksInfo.push({
                             label: cf.name,
                             children: cfInfo,
+                            isColumnFamily: true,
                             collapsed: true
-//                        children: [{
-//                            "label": "bb"
-//                        }]
                         });
+                        ksInfo.sort(labelSort);
                     });
                     for (var k in ks) {
                         k != 'name' && k != 'cf_defs' && ksInfo.push({
@@ -62,17 +84,58 @@ angular.module("cassandraWeb", ['angularTreeview'])
 
         $scope.treeData = [];
 
-    }).directive('resizable', function () {
-        return {
-            restrict: 'A',
-            scope: {
-                callback: '&onResize'
-            },
-            link: function postLink(scope, elem, attrs) {
-                elem.resizable();
-                elem.on('resizestop', function (evt, ui) {
-                    if (scope.callback) { scope.callback(); }
-                });
+    }).directive('resizer', function($document) {
+
+        return function($scope, $element, $attrs) {
+
+            $element.on('mousedown', function(event) {
+                event.preventDefault();
+
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+
+                if ($attrs.resizer == 'vertical') {
+                    // Handle vertical resizer
+                    var x = event.pageX;
+
+                    if ($attrs.resizerMax && x > $attrs.resizerMax) {
+                        x = parseInt($attrs.resizerMax);
+                    }
+
+                    $element.css({
+                        left: x + 'px'
+                    });
+
+                    angular.element(document.querySelector($attrs.resizerLeft)).css({
+                        width: x + 'px'
+                    });
+                    angular.element(document.querySelector($attrs.resizerRight)).css({
+                        left: (x + parseInt($attrs.resizerWidth)) + 'px'
+                    });
+
+                } else {
+                    // Handle horizontal resizer
+                    var y = window.innerHeight - event.pageY;
+
+                    $element.css({
+                        bottom: y + 'px'
+                    });
+
+                    angular.element(document.querySelector($attrs.resizerTop)).css({
+                        bottom: (y + parseInt($attrs.resizerHeight)) + 'px'
+                    });
+                    angular.element(document.querySelector($attrs.resizerBottom)).css({
+                        height: y + 'px'
+                    });
+                }
+            }
+
+            function mouseup() {
+                $document.unbind('mousemove', mousemove);
+                $document.unbind('mouseup', mouseup);
             }
         };
     });
